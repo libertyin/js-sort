9245e6e68c8e2b4d68b42dde552334ba173ae4c2
libertyin <anna.pretty40@gmail.com>
2018-11-28 14:44:31 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,32 @@
+'use-strict'
+
+   var btnsBox =  $('.js-btns');
+   var btnItems = btnsBox.find('button');
+   var contantImg = $('.js-img');
+
+   btnItems.on('click', sortImg);
+   
+   function sortImg() {
+
+    var currentBtn = $(this).data('color');
+    console.log(currentBtn);
+
+    contantImg.each(function(index , elem) {
+
+        $(this).css('display', 'block');
+    });
+
+    contantImg.each(function(index , elem){
+
+        currentItem = $(this).attr('id');
+
+        if(currentItem!=currentBtn && currentBtn!='all') {
+            $(this).css({
+                'display':'none'
+            })
+        }
+    });
+    
+
+   }
+
